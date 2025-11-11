1cb10911110dd4df27164da04f3abb7009d6a87f
黃子熙 <jasonwth0928@gmail.com>
2025-11-12 03:28:45 +0800

    Add files via upload

--- AIFinanceApp/metro.config.js
+++ AIFinanceApp/metro.config.js
@@ -1,9 +1,19 @@
 // metro.config.js
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+const fs = require('fs'); // 引入 Node.js 檔案系統模組
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// 如果您需要新增任何額外的自訂配置，請在這裡修改 config 物件
+// ---------------------------------------------------
+// Wasm 載入修復: 告訴 Metro 如何將 .wasm 檔案載入為 base64 字串
+// ---------------------------------------------------
 
-module.exports = config;
+// 1. 確保 Wasm 被列為源文件擴展名，並從資產列表中移除
+const assetExts = config.resolver.assetExts;
+config.resolver.assetExts = assetExts.filter((ext) => ext !== 'wasm');
+config.resolver.sourceExts.push('wasm');
+
+// 2. 創建自定義轉換器來處理 .wasm 檔案
+config.transformer.get;//
